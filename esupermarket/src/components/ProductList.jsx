42daa52f81b17023e80d3f6537d13946ebbfe05f
mohdazeem0 <mohdazm007@gmail.com>
2021-08-21 16:06:43 +0530

    update

--- esupermarket/src/components/ProductList.jsx
+++ esupermarket/src/components/ProductList.jsx
@@ -15,17 +15,7 @@ export const ProductList = () => {
       <br />
       <h1 style={{ textAlign: "center", fontSize: "60px" }}>Products</h1>
       <Col>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img></Card.Img>
-          <Card.Body>
-            <Card.Title>Palak Veggies</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <ProductItem />
-          </Card.Body>
-        </Card>
+        <Card style={{ width: "18rem" }}>{prod.map(item)}</Card>
       </Col>
     </Row>
   );
